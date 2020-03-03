0e37922abd09e9fe3126ccaa48880e33f25a9f9c
aqilfiqran <gamingaft@gmail.com>
2020-03-04 01:36:06 +0700

    latest version

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# scrap-in-python
+# Scrap in Python
+
+## Run Program
+- Example : `python Scrap.py out/namefile.txt -p 100`
+![example](./doc/example.PNG)
+- More info : `python Scrap.py -h`
+![help](./doc/help.PNG)
+
+## Requirement
+- python3 (recommended)
+- `pip install -r requirements.txt`
+
+## Sources
+- [beatifulsoup4](https://www.crummy.com/software/BeautifulSoup/bs4/doc/)
---
+++ Scrap.py
@@ -0,0 +1,76 @@
+from bs4 import BeautifulSoup
+from datetime import datetime, timedelta
+from pathlib import Path
+import sys
+import requests
+import argparse
+
+# scrapping url method
+
+
+def scrap(data):
+    soup = BeautifulSoup(requests.get(data[0]).text, 'html.parser')
+    title = soup.select_one('.read__title').get_text()
+    credit = soup.select_one('.read__credit')
+    editor = credit.findAll('a')[1].get_text()
+    data[1].write(f'{editor}||{title}\n')
+
+
+# argument documentary
+arg = argparse.ArgumentParser()
+arg.add_argument("directory", help="path output file all")
+option = arg.add_mutually_exclusive_group()
+option.add_argument("-p", "--page-limit",
+                    help="limit file by total of rows", type=int)
+option.add_argument("-d", "--day-limit",
+                    help="limit file by days backward", type=int)
+args = vars(arg.parse_args())
+
+# dinamic link
+indexlink = "https://indeks.kompas.com/?site=all&date="
+
+# get time of today
+date = datetime.today()
+
+# number of day and limit
+day, day_limit = 1, args['day_limit'] if args['day_limit'] != None else None
+
+# number of page and limit
+page, page_limit = 0, args['page_limit'] if args['page_limit'] != None else None
+
+path = Path()/args['directory']
+with open(path, 'w') as file:
+    # looping to get dynamic link by time
+    while True:
+        link = f'{indexlink}{date.strftime("%Y-%m-%d")}'
+        # looping to get all detail link
+        while True:
+            print(f'Getting url from : {link}')
+            soup = BeautifulSoup(requests.get(
+                link).text.encode('utf-8'), 'html.parser')
+            print("Url found      : ", len(soup.select('.article__title')))
+            for url in soup.select('.article__title'):
+                urls = url.find('a')['href']
+                try:
+                    # clean url and put on directory file
+                    scrap([urls, file])
+                except (AttributeError, IndexError):
+                    pass
+                page += 1
+                if page == page_limit:
+                    sys.exit(f'Program reach maximum {page_limit} page')
+            else:
+                try:
+                    if(soup.select_one(
+                            '.paging__link--next')):
+                        link = soup.select_one(
+                            '.paging__link--next')['href']
+                        continue
+                    else:
+                        break
+                except TypeError:
+                    pass
+        if day == day_limit:
+            sys.exit(f'Program reach maximum {day_limit} day')
+        date += timedelta(days=-1)
+        day += 1
